29214e8b57a6300b8cb519ec97d06d4f9a1a0097
John Roller <jtroller33@gmail.com>
2022-11-30 21:59:39 -0600

    expandable card finished

--- framer-motion-expandable-card/expandable-card/src/App.js
+++ framer-motion-expandable-card/expandable-card/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// https://www.youtube.com/watch?v=rZ6iu5FQr2c
+// https://www.youtube.com/@developedbyed
+
+
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <motion.div 
+        transition={{ layout: { duration: 1, type: "spring" } }} 
+        layout 
+        onClick={() => setIsOpen(!isOpen)} 
+        className='card'
+        style={{borderRadius: "2rem", boxShadow: '0px 10px 30px rgba(0,0,0, 0.5)' }}
         >
-          Learn React
-        </a>
-      </header>
+
+        <motion.h2 layout="position">Motion ✨</motion.h2>
+        {isOpen && (
+          <motion.div 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className='expandingDiv'
+            >
+            <p>aasdfasjd;ofinbasoifbawoibfw2o043rg2q0o89i3gr8029oqg3f09o8gq4e</p>
+            <p>as;ldkfhaoibg0o2wq3ght21093ytr02=9 htoiWQHEGNFOIWARBVOIRWEBG</p>
+          </motion.div>
+        )}
+      </motion.div>
+
     </div>
   );
 }
 
 export default App;
+
